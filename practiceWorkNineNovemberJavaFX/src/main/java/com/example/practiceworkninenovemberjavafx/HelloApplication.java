9c421ce35f00e2096e5af66c14c2288930a2cb00
DasAdErt <78647864+DasAdErt@users.noreply.github.com>
2023-11-09 20:08:33 +0300

    Add files via upload

--- practiceWorkNineNovemberJavaFX/src/main/java/com/example/practiceworkninenovemberjavafx/HelloApplication.java
+++ practiceWorkNineNovemberJavaFX/src/main/java/com/example/practiceworkninenovemberjavafx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.practiceworkninenovemberjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        String iconUrl = "https://media.discordapp.net/attachments/1125898813912125483/1172219144884719657/icon.png?ex=655f8526&is=654d1026&hm=da5ddd44cb899a617f71c057ef3d81cb73ebabe1e94afd43513e5e059bc5707c&=&width=480&height=480";
+        Image applicationIcon = new Image(iconUrl);
+        stage.getIcons().add(applicationIcon);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 321);
         stage.setTitle("Program");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- practiceWorkNineNovemberJavaFX/src/main/java/com/example/practiceworkninenovemberjavafx/HelloController.java
+++ practiceWorkNineNovemberJavaFX/src/main/java/com/example/practiceworkninenovemberjavafx/HelloController.java
@@ -2,6 +2,11 @@ package com.example.practiceworkninenovemberjavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class HelloController {
 
@@ -15,11 +20,57 @@ public class HelloController {
     TextField answer;
 
     @FXML
-    ChoiceBox choiceSystem;
+    ChoiceBox<String> choiceSystem;
+
+    @FXML
+    private Label computerNameLabel;
+
+    @FXML
+    private ImageView avatarImageView;
+
+    @FXML
+    public void initialize() {
+        choiceSystem.getItems().addAll("Бинарная", "Восьмеричная", "Шестнадцатеричная");
+        choiceSystem.setValue("Бинарная");
+
+        try {
+            String computerName = InetAddress.getLocalHost().getHostName();
+            computerNameLabel.setText(computerName);
+        } catch (UnknownHostException e) {
+            computerNameLabel.setText("Неизвестный компьютер");
+        }
+
+        Image avatarImage = new Image("https://media.discordapp.net/attachments/1125898813912125483/1172216042165117019/IMG_2731.JPG?ex=655f8243&is=654d0d43&hm=121c3ea49a366dd2e30a7d8c2aadb0dc20528f6dbc5319742baa8e7041d4665f&=&width=477&height=477");
+        avatarImageView.setImage(avatarImage);
+    }
 
     @FXML
     public void onButtonClick() {
-        int number = Integer.parseInt(inputNumber.getText());
+        String selectedSystem = choiceSystem.getValue();
+        String input = inputNumber.getText();
+        int decimalValue;
 
+        try {
+            decimalValue = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            answer.setText("Введите корректное число!");
+            return;
+        }
+
+        String result = convertToSystem(decimalValue, selectedSystem);
+        answer.setText(result);
+    }
+
+    private String convertToSystem(int decimalValue, String system) {
+        switch (system) {
+            case "Бинарная":
+                return Integer.toBinaryString(decimalValue);
+            case "Восьмеричная":
+                return Integer.toOctalString(decimalValue);
+            case "Шестнадцатеричная":
+                return Integer.toHexString(decimalValue).toUpperCase();
+            default:
+                return "Неизвестная система";
+        }
     }
 }
